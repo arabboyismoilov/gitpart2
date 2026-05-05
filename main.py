0274e52f172709187829cb04b957a18cfbb1579f
Arabboy Ismoilov <arabboyismoilov27@gmail.com>
2026-05-05 10:13:43 +0500

    start command implement qilindi

--- main.py
+++ main.py
@@ -3,3 +3,13 @@
 # commit history
 
 # branchlar
+
+# Nodirbek Temurbek MuhammadSodiq
+
+# Nodirbek start handler yozishga masul
+# Temurbek help handler yozishga masul
+# MuhammadSodiq settings handler yozishga masul
+
+# main   #feature/startCommand
+
+# start command implement qilindi
